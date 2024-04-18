f0c6c3534e5cc76256230921562ff0629e9b6382
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-04-18 11:14:23 +0000

    Deploying to gh-pages from @ MatacheAlexandru/Project-TeamWork@f7304f75266e043ed69b434b69aa386ce9ebc6a6 🚀

--- index.36b02d02.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn&&o.closeModalBtn&&o.modal&&(o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e))})();
-//# sourceMappingURL=index.36b02d02.js.map
--- index.9b7282d9.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn&&o.closeModalBtn&&o.modal&&(o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e))}();
-//# sourceMappingURL=index.9b7282d9.js.map
