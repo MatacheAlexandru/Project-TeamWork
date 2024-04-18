0a9529c28c192e1c91516f40eb365cb08bb2001e
DumLiviu <158556659+DumLiviu@users.noreply.github.com>
2024-04-18 23:27:45 +0300

    Adaugare About-Section

--- src/about-modal.js
+++ src/about-modal.js
@@ -1,14 +1,32 @@
-(() => {
+// (() => {
+//     const refs = {
+//         openModalBtn: document.querySelector("[data-modal-open]"),
+//         closeModalBtn: document.querySelector("[data-modal-close]"),
+//         modal: document.querySelector("[data-modal]"),
+//     };
+
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//     function toggleModal() {
+//         refs.modal.classList.toggle("is-hidden");
+//     }
+// })();
+document.addEventListener('DOMContentLoaded', function() {
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
         closeModalBtn: document.querySelector("[data-modal-close]"),
         modal: document.querySelector("[data-modal]"),
     };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+    }
 
     function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
+        if (refs.modal) {
+            refs.modal.classList.toggle("is-hidden");
+        }
     }
-})();
+});
