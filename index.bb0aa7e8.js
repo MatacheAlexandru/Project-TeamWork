5c47433302883956121c83e8834bef55151e8a6a
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-04-19 15:37:33 +0000

    Deploying to gh-pages from @ MatacheAlexandru/Project-TeamWork@234728cbdfb31aa3086f921631e2a421bc12e25f 🚀

---
+++ index.bb0aa7e8.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".read-more-btn"),t=document.querySelector(".close-btn"),d=document.querySelector("[data-modal-target]");e.addEventListener("click",(function(){d.classList.remove("hidden-modal")})),t.addEventListener("click",(function(){d.classList.add("hidden-modal")}))}));
+//# sourceMappingURL=index.bb0aa7e8.js.map
